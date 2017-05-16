93b06986fa6c11ef28005c83746deff41903ee2a
Flavio Curella <flavio.curella@gmail.com>
2017-05-16 12:19:23 -0500

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 here = os.path.abspath(os.path.dirname(__file__))
 README = io.open(os.path.join(here, 'README.rst'), encoding="utf8").read()
 
